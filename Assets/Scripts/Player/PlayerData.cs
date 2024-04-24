3d80358a507d5512a5e834df2be30be2cf5d3855
KawaRyuu <167944356+KawaRyuu@users.noreply.github.com>
2024-04-24 20:13:26 +0900

    プレイヤーが攻撃受けたときの処理（1）

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -8,23 +8,25 @@ using UnityEngine;
 //プレイヤーの基礎的なデータ
 public class PlayersData
 {
-    private string Id = ("");     //おそらく名前
-    private int Hp = 3;           //体力（耐久値）
-    private int Attack = 1;       //攻撃
-    private int NomberBox = 4;    //4つの数を保持する
-    private float Speed = 1.0f;   //移動速度
+    public string Id = ("");     //おそらく名前
+    public int Hp = 3;           //体力（耐久値）
+    public int Attack = 1;       //攻撃
+    public int NomberBox = 4;    //4つの数を保持する
+    public float Speed = 1.0f;   //移動速度
 
-    private float CoolTime = 4f;
+    public float CoolTime = 4f;
     //技や奪取（交換）を含めたクールタイム全般
 
-    private float invincibility = 0.5f;     //ダメージ喰らった際の無敵時間
-    private int  RandomNomber = 0;          //自分の数を出すランダム数をここに
-    private bool SwoonFlg = false;          //気絶フラグ
-    private bool RecoveryTime_Flag = false; //回復タイムに入るフラグ
+    public float invincibility = 0.5f;     //ダメージ喰らった際の無敵時間
+    public int RandomNomber = 0;           //自分の数を出すランダム数をここに
+    public bool SwoonFlg = false;          //気絶フラグ
+    public bool RecoveryTime_Flg = false;  //回復タイムに入るフラグ
+    public bool Invincibility_Flg = false; //無敵フラグ
 
 }
 public class PlayerData : MonoBehaviour
 {
+
     private void Start()
     {
     }
