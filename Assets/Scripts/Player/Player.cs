d0333b9368f694ee14dfc914b1e3e2e8cf400e50
KawaRyuu <167944356+KawaRyuu@users.noreply.github.com>
2024-04-28 18:13:13 +0900

    敵から攻撃受けたあと、5秒の間で攻撃が来ないなら回復する処理と 気絶中とそのあとの処理を完成させました。

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -21,26 +21,31 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
-        
-        //Playerの基本の動き
-        if (Input.GetKey("left"))
-        {
-            position.x -= info.Speed * Time.deltaTime;          //左方向
-        }
-        else if(Input.GetKey("right"))
-        {
-            position.x += info.Speed * Time.deltaTime;          //右方向
-        }
-        else if(Input.GetKey("up"))
-        {
-            position.y += info.Speed * Time.deltaTime;          //上方向
-        }
-        else if(Input.GetKey("down"))
-        {
-            position.y -= info.Speed * Time.deltaTime;          //下方向
-        }
-        transform.position = position;
 
-        Debug.Log(("今のスピードは") + info.Speed);
+        //Playerの基本の動き
+        //もし気絶中なら行動不可
+        if (!info.Swoon_Flg)
+        {
+            if (Input.GetKey("left"))
+            {
+                position.x -= info.Speed * Time.deltaTime;          //左方向
+            }
+            else if (Input.GetKey("right"))
+            {
+                position.x += info.Speed * Time.deltaTime;          //右方向
+            }
+            else if (Input.GetKey("up"))
+            {
+                position.y += info.Speed * Time.deltaTime;          //上方向
+            }
+            else if (Input.GetKey("down"))
+            {
+                position.y -= info.Speed * Time.deltaTime;          //下方向
+            }
+            transform.position = position;
+        }
+        //Debug.Log(("今のスピードは") + info.Speed);
+        Debug.Log("今の体力は" + info.Hp);
+        //Debug.Log("無敵フラグは:" + info.Invincibility_Flg);
     }
 }
--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -8,42 +8,85 @@ using UnityEngine;
 public class PlayerData : MonoBehaviour
 {
 
-    public string Id = ("");     //おそらく名前
-    public int Hp = 3;        //体力（耐久値）
-    public int Attack = 1;        //攻撃
+    public string Id = ("");         //おそらく名前
+    public int Hp = 3;               //体力（耐久値）
+    public int Attack = 1;           //攻撃
     public int NomberBox = 4;        //4つの数を保持する
-    public int RandomNomber = 0;        //自分の数を出すランダム数をここに
-    public float Speed = 5.0f;     //移動速度
+    public int RandomNomber = 0;     //自分の数を出すランダム数をここに
+    public float Speed = 5.0f;       //移動速度
     public float CoolTime = 4f;
     //技や奪取（交換）を含めたクールタイム全般
 
-    public float invincibility = 0.5f;     //ダメージ喰らった際の無敵時間
-    public bool SwoonFlg = false;    //気絶フラグ
-    public bool StunFlg = false;     //スタンフラグ
-    public bool RecoveryTime_Flg = false;    //回復タイムに入るフラグ
-    public bool Invincibility_Flg = false;    //無敵フラグ
-    public bool ExchangeTakeover_Flg = false;    //交換奪取するフラグ
-    public bool BluntFootEffect_Flg = false;    //鈍足効果のフラグ
+    public float invincibility = 0.5f;          //ダメージ喰らった際の無敵時間
+    public bool Swoon_Flg            = false;   //気絶フラグ
+    public bool Stun_Flg             = false;   //スタンフラグ
+    public bool RecoveryTime_Flg     = false;   //回復タイムに入るフラグ
+    public bool Invincibility_Flg    = false;   //無敵フラグ
+    public bool ExchangeTakeover_Flg = false;   //交換奪取するフラグ
+    public bool BluntFootEffect_Flg  = false;   //鈍足効果のフラグ
 
-    public float inv_count = 0.0f;      //無敵時間中のカウント
-    public float blunt_count = 0.0f;    //鈍足中のカウント
+    public float inv_count   = 0.0f;            //無敵時間中のカウント
+    public float blunt_count = 0.0f;            //鈍足中のカウント
+    public float hael_count  = 0.0f;            //回復中のカウント
+    public float swoon_count = 0.0f;            //気絶中のカウント
 
     //初期化
     void Start()
     {
         Invincibility_Flg   = false;
-        SwoonFlg            = false;
+        Swoon_Flg           = false;
         BluntFootEffect_Flg = false;
-        inv_count = 0.0f;
+        inv_count   = 0.0f;
         blunt_count = 0.0f;
+        hael_count  = 0.0f;
+        swoon_count = 0.0f;
     }
 
      void Update()
     {
+        /**********気絶の処理*************/
+        //もし気絶フラグがtrue且つカウントが1.5秒以下なら
+        if (Swoon_Flg && swoon_count <= 1.5f)
+        {
+            Debug.Log("気絶now");
+            swoon_count += Time.deltaTime;    //カウントの加算
+        }
+        else if (swoon_count >= 1.5)          //もしカウントが1.5秒を超えたら
+        {
+            swoon_count = 0.0f;               //気絶カウントをリセット
+            Swoon_Flg   = false;              //気絶フラグをfalseに変える
+            Hp          = 3;                  //HPは強制で全回復
+        }
+
+
+        /**********回復の処理*************/
+        //もし回復フラグがtrue且つカウントが5.0秒以下なら
+        //※次の攻撃が来るのが5秒以降になるなら全回復する。
+        if (RecoveryTime_Flg && hael_count <= 5.0f)
+        {
+            hael_count += Time.deltaTime;
+            Debug.Log("回復中");
+            //Debug.Log("回復カウントは" + hael_count);
+        }
+        //もし回復のカウントが5秒を超えたなら
+        else if (hael_count > 5.0)
+        {
+            Debug.Log("回復");
+            Hp = 3;                                 //Hpを回復する。
+            hael_count = 0;                         //カウントリセット
+            RecoveryTime_Flg = false;               //回復フラグOFF
+        }
+            
+
+
+        /**********無敵の処理*************/
+
         //無敵フラグがON且つもし無敵時間が0.5秒以下なら
         if (Invincibility_Flg && invincibility >= inv_count)
         {
             inv_count += Time.deltaTime;
+            RecoveryTime_Flg = true;                //回復フラグをONにする。
+            Debug.Log("回復フラグ通ったよ");
         }
         else
         {
@@ -52,12 +95,13 @@ public class PlayerData : MonoBehaviour
             inv_count = 0;
         }
 
+        /**********鈍足効果の処理************/
+
         //もし鈍足効果のフラグがfalseなら通常の速度
         if (!BluntFootEffect_Flg)
         {
             Speed = 5.0f;
         }
-
 
         //鈍足効果がtrue且つ鈍足カウントが2.0秒以下なら
         if (BluntFootEffect_Flg && blunt_count <= 2.0)
@@ -70,24 +114,35 @@ public class PlayerData : MonoBehaviour
             blunt_count = 0;
         }
     }
+
+
+    /************当たった時の処理(何かの当たった時)*****************/
     private void OnTriggerEnter2D(Collider2D other)
     {
         //敵の攻撃（EnemyAttackというtag）に触れたとき
         if (other.gameObject.tag == "EnemyAttack")
         {
-            //もし無敵状態じゃない且つ気絶してないときなら
-            if (!Invincibility_Flg && !SwoonFlg)
-            {
-                Hp -= Attack;         //体力がAttackの攻撃参照で減る
-                Invincibility_Flg = true;  //無敵フラグON
-            }
 
-            //もし体力が0以下になったら
-            if (Hp >= 0)
+            //もし無敵状態じゃない且つ気絶してないときなら
+            if (!Invincibility_Flg && !Swoon_Flg)
             {
-                //気絶処理
-                SwoonFlg = true;
-                Hp = 3;
+                //フラグがtrueの時、追撃が飛んで来たら
+                if (RecoveryTime_Flg)
+                {
+                    //回復のキャンセル
+                    RecoveryTime_Flg = false;
+                    hael_count = 0.0f;
+                }
+
+                Hp -= Attack;               //体力がAttackの攻撃参照で減る
+                Invincibility_Flg = true;   //無敵フラグON
+            }
+          
+            //もし体力が0以下になったら
+            if (Hp <= 0)
+            {
+                //気絶フラグON
+                Swoon_Flg = true;
             }
         }
 
@@ -98,5 +153,10 @@ public class PlayerData : MonoBehaviour
             Speed = 2.5f;
             BluntFootEffect_Flg = true;
         }
+    }
+
+    //移動速度UP関数
+    public void SpeedUp()
+    {
     }
 }
