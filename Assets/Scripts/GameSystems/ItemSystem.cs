f8959f8b63b73052c92a1e255b55d6748e2de476
3ka2ki <oibc34003@ojs.ac.jp>
2024-04-28 18:23:22 +0900

    気絶アイテムの処理(仮)完成 数字変更系のアイテムにコメントを超追加

--- Assets/Scripts/GameSystems/ItemSystem.cs
+++ Assets/Scripts/GameSystems/ItemSystem.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,31 +9,81 @@ using UnityEngine;
 //プレイヤーのステータス参照
 public class Item
 {
-    //どのアイテムを所持しているかの変数
+    PlayerData Pl;
+
+    /*アイテムメモ：同じ種類のアイテムは所持不可能*/
+
+    /*どのアイテムを所持しているかの変数*/
+    public bool Change_min_Flg      = false;//自動的にランダム奪取が低い数へ適用される。
+    public bool Num_Random_Flg      = false;//4枚の数のうち、一番低い数が１～９にランダムで変わる。
+    public bool Nummin_one_Flg      = false;//4枚の数の中で1以外の小さい数が1枚1に変わる。
+    public bool Change_random_Flg   = false;//自身の数字をランダムな人と交換（交換している人同士は見える）
+    public bool Speed_up_Flg        = false;//自身移動速度UP（3秒間）
+    public bool Spdown_Flg          = false;//相手に投げ飛ばして移動速度Down（2秒間）
+    public bool Critical_Flg        = false;//一撃気絶のアイテム
 
 
-    //アイテムの処理をする関数
 
-    //アイテムで無敵ではないが、自動的にランダム奪取が低い数へ適用される。
-    public void Change_min(){}
+    /*アイテムの処理をする関数*/
+
+    //自動的にランダム奪取が低い数へ適用される。
+    public int Change_min()
+    {
+        int min = 9;
+        Pl.GetComponent<PlayerData>();
+        //数字が入っている配列の中で一番小さい数字を取らせる
+        //一番小さい数字を算出
+
+
+        return min;
+        /*
+         * メモ：このアイテム使用した後に
+         * 自身の数字を変更するアイテムを使用したら
+         * 返した配列番号が最小ではない値になる可能性がある
+         * 
+         *→解決案：数字変更するアイテム使用時にもう一度この関数を呼び出す
+         */
+
+    }
 
     //4枚の数のうち、一番低い数が１～９にランダムで変わる。
-    public void NUM_Random() {}
+    public void NUM_Random()
+    {
+        
+    }
 
     //4枚の数の中で1以外の小さい数が1枚1に変わる。
-    public void NumMin_One() {}
+    public void NumMin_One()
+    {
+        
+    }
 
     //自身の数字をランダムな人と交換（交換している人同士は見える）
-    public void Change_Random() { }
+    public void Change_Random()
+    {
+        
+    }
 
     //自身移動速度UP（3秒間）
-    public void Speed_Up() { }
+    /*アイテム使ったときに移動速度アップの関数を持ってくる*/
+    public void Speed_Up()
+    {
+        
+    }
 
     //相手に投げ飛ばして移動速度Down（2秒間）
-    public void SpDown() { }
+    /*PlayerDataに鈍足処理あり→鈍足タグついたオブジェクト召喚プログラムへ*/
+    public void SpDown()
+    {
+        
+    }
 
     //一撃気絶のアイテム
-    public void Critical() {  }	        
+    public void Critical()
+    {
+        Pl.GetComponent<PlayerData>();
+        Pl.Attack = 3;
+    }
 
 }
 
