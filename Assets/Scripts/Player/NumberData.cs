b0f8f18572e1edcc7ce071708f477492b441609d
KawaRyuu <167944356+KawaRyuu@users.noreply.github.com>
2024-05-01 15:49:30 +0900

    Game画面をガラッと変えました。(仮です)それと自分の持っている数を表示するようにしました。

--- Assets/Scripts/Player/NumberData.cs
+++ Assets/Scripts/Player/NumberData.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class NumberData : MonoBehaviour
 {
     //自身が持っているNumber
     public int []MyNumber = {0,0,0,0};
+    public Text MyNnm1;                  //UIの表示
+    public Text MyNnm2;                  //UIの表示
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +27,11 @@ public class NumberData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //自分の数をUnityで表示
+        MyNnm1.text = MyNumber[0].ToString() 
+            +(" ・ ")+MyNumber[1].ToString();
+
+        MyNnm2.text = MyNumber[2].ToString()
+            + (" ・ ") + MyNumber[3].ToString();
     }
 }
