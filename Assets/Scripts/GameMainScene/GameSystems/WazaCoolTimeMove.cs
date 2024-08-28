05a89b08cc4096c6b8dc3191ef83e30f1838a294
KawaRyuu <167944356+KawaRyuu@users.noreply.github.com>
2024-08-28 15:16:49 +0900

    コメントアウトの追加、1ブランチ前の処理2つ目を保留状態にしました。

--- Assets/Scripts/GameMainScene/GameSystems/WazaCoolTimeMove.cs
+++ Assets/Scripts/GameMainScene/GameSystems/WazaCoolTimeMove.cs
@@ -7,9 +7,13 @@ public class WazaCoolTimeMove : MonoBehaviour
 {
     PlayerData CoolTimeData;
     public Image UIobj;
+    //public Image UIobj2;
+
     public bool roop;
+    public bool roop2;
+
     float Tec1_countTime = 1.0f;
-    float Tec2_countTime = 0.0f;
+    float Tec2_countTime = 1.0f;
 
     void Start()
     {
@@ -18,25 +22,36 @@ public class WazaCoolTimeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //もし技1のクールタイムが発生したら
         if (CoolTimeData.Tec01_CoolTime > 0)
         {
             roop = true;
         }
         else
         {
+            //クールタイムが0秒になったら円をもとに戻す
             roop = false;
             UIobj.fillAmount = 1;
         }
 
+        //roopがtrueなら
         if (roop)
         {
-            
+            //技1の待ち時間演出を始める。
             UIobj.fillAmount -= 1.0f / Tec1_countTime * Time.deltaTime;
         }
+        else if(roop2)
+        {
+            //技2の待ち時間演出を始める。
+            //UIobj2.fillAmount -= 1.0f / Tec2_countTime * Time.deltaTime;
+        }
 
+        //もし1周したらもう一度もとに戻しループさせる
         if (UIobj.fillAmount <= 0)
         {
             UIobj.fillAmount = 1;
         }
+        /*else if (UIobj2.fillAmount <= 0)
+            UIobj2.fillAmount = 1;*/
     }
 }
