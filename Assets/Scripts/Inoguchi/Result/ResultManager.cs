a47c1bb51e49d2136932df2fdb02bf246254944c
tora-3750 <oibc34173@ojs.ac.jp>
2024-09-02 13:30:28 +0900

    リザルトシーン

---
+++ Assets/Scripts/Inoguchi/Result/ResultManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ResultManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] player_winner_panels = new GameObject[4];
+
+    private void Start()
+    {
+        
+    }
+
+    void DisplayResult()
+    {
+
+    }
+
+}
