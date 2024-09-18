157693d18ee3e4cf4a65aa90f00d9d6bf6ed4be9
KawaRyuu <167944356+KawaRyuu@users.noreply.github.com>
2024-09-18 16:50:02 +0900

    タイトル画面のカーソル完成

--- Assets/Scripts/TitleScripts/ButtonScripts.cs
+++ Assets/Scripts/TitleScripts/ButtonScripts.cs
@@ -9,6 +9,9 @@ public class ButtonScripts : MonoBehaviour
     //PlayerのInputSystem
     PlayerInput playerInput;
 
+    public GameObject SentakuWaku1;
+    public GameObject SentakuWaku2;
+
     private void Awake()
     {
         TryGetComponent(out playerInput);
@@ -16,17 +19,39 @@ public class ButtonScripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SentakuWaku1.SetActive(false);
+        SentakuWaku2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //コントローラの操作[B](決定ボタン)
         bool SelectButton = playerInput.actions["SelectButton"].WasPressedThisFrame();
 
+        //InputSystemのactionmapからMoveを取得
+        Vector2 MoveLeftButton = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        //特定の操作を確認したら
         if (SelectButton)
         {
-            ChangeScene();
+            //ChangeScene();
+            //ボタンを押すとき特定の動きができるようにする。
+        }
+
+        //もしYが0より小さい場合
+        if (MoveLeftButton.y < 0)
+        {
+            //スタートボタンに枠を表示
+            SentakuWaku1.SetActive(true);
+            SentakuWaku2.SetActive(false);
+        }
+        //もしYが0より大きい場合
+        else if (MoveLeftButton.y > 0)
+        {
+            //歯車に選択枠を表示
+            SentakuWaku2.SetActive(true);
+            SentakuWaku1.SetActive(false);
         }
     }
 
